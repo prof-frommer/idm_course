7c1ca749718157eeeb49dac2e13a9e98c45d20a3
Frommer <Ian.Frommer@uscga.edu>
2026-01-20 14:17:32 -0500

    hw02 posted, lec03 files posted

---
+++ HW/hw02-pulp-python-amply/diet_skeleton.py
@@ -0,0 +1,18 @@
+def diet_lp(file_name):
+    ### YOUR CODE HERE	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+if __name__ == "__main__":  
+    dat_file = 'ward_room.dat'
+    diet_lp(dat_file)
